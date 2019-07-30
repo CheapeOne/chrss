ca2b4f4b3478a5a5f0f89266419dca76b7c18b74
Christopher Cheape <cheapeone@Christophers-MacBook-Pro.local>
2019-07-29 23:53:46 -0400

    got things working

---
+++ src/api/httpStatusCodes.ts
@@ -0,0 +1,9 @@
+// tslint:disable-next-line typedef variable-name (Acts like a type with static properties.)
+export const httpStatusCodes = {
+    BAD_REQUEST: 400,
+    CONFIGURATION_ERROR: 500.19,
+    FORBIDDEN: 403,
+    INTERNAL_SERVER_ERROR: 500,
+    NOT_FOUND: 404,
+    OK: 200
+  };
